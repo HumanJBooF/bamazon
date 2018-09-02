6b5f46d55768a736540362f8ef7c85d6a8e9a27a
Joshua LeBoeuf <humanjboof@gmail.com>
2018-09-02 12:36:02 -0400

    password

--- provisioner.sh
+++ provisioner.sh
@@ -21,7 +21,7 @@ sudo apt-get install libcap2-bin
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
 #install mySql
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password SuperSecretPasswordHere'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password SuperSecretPasswordHere'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password bison'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password bison'
 
 apt-get install -y mysql-server

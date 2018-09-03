8d925b48cb1c2a0456d572d9c6637ee1126bd54f
Joshua LeBoeuf <humanjboof@gmail.com>
2018-09-03 12:11:38 -0400

    testing dotenv for DB

--- code/bamazonCustomer.js
+++ code/bamazonCustomer.js
@@ -1,17 +1,18 @@
+require('dotenv').config
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cli = require('cli-table');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: '', // may put password in another file so for now leaving it out
+    password: process.env.DB_PASS,
     database: 'bamazon'
 });
 
-// connection.connect(err => {
-//     if (err) throw err;
-
-// });
+connection.connect(err => {
+    if (err) throw err;
+    connection.end();
+});
 

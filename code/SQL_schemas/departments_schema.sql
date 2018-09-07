5eb465a1783d30ad7968eca8f9013bf0717985be
Joshua LeBoeuf <humanjboof@gmail.com>
2018-09-07 17:17:35 -0400

    added a validate to check input id is in db on customer(will add others)

--- code/SQL_schemas/departments_schema.sql
+++ code/SQL_schemas/departments_schema.sql
@@ -5,7 +5,5 @@ CREATE TABLE departments (
     department_id INTEGER(200) NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(200) NOT NULL,
     over_head_cost DECIMAL(10,2) NOT NULL,
-    product_sales DECIMAL(10,2) NOT NULL,
-    total_profits INTEGER(200),
     primary key (depatment_id)
 );
--- code/bamazonCustomer.js
+++ code/bamazonCustomer.js
@@ -3,41 +3,53 @@ const con = require('./connection.js');
 const mysql = require('mysql');
 const Table = require('cli-table-redemption');
 const chalk = require('chalk');
+let bold = chalk.green.bold; // chalk npm for colors
 
 const askQuestion = () => {
+    con.query(`SELECT * FROM products`, (err, res) => { //querying first so we can get the length of our DB to match the input
+        if (err) throw err;
+        let dbLength = res.length
+        console.log(dbLength);
+        inquirer.prompt([{
+            type: 'input',
+            name: 'item_id',
+            message: 'What item ID would you like to buy?',
+            validate: (value) => {
+                if (value > dbLength) {
+                    console.log(chalk`{bold \r\n We don't have a product with ID: ${value}} Please try again \r\n`);
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many would you like to purchase?',
+        }]).then(answers => {
+            let id = answers.item_id; // making this a little smaller
+            // look for the ID number
+            con.query(`SELECT * FROM products WHERE item_id=${id}`, (err, res) => {
+                if (err) throw err;
+                // console.log(res)
 
-    inquirer.prompt([{
-        type: 'input',
-        name: 'item_id',
-        message: 'What item ID would you like to buy?'
-    },
-    {
-        type: 'input',
-        name: 'quantity',
-        message: 'How many would you like to purchase?'
-    }]).then(answers => {
-        let id = answers.item_id; // making this a little smaller
-        // look for the ID number
-        con.query(`SELECT * FROM products WHERE item_id=${id}`, (err, res) => {
-            if (err) throw err;
-            // console.log(res)
+                let amountWanted = answers.quantity; // Put how many the user wants into a variable
+                let inStock = res[0].stock_quantity; // how many is in stock currently
+                let item = res[0].product_name; // name of the item the user wants
+                let price = res[0].price; // price of that item
 
-            let amountWanted = answers.quantity; // Put how many the user wants into a variable
-            let inStock = res[0].stock_quantity; // how many is in stock currently
-            let item = res[0].product_name; // name of the item the user wants
-            let price = res[0].price; // price of that item
-
-            if (amountWanted <= inStock) { // if the amount wanted is less than or equal to whats in stock
-                let newTotal = inStock - amountWanted; // subtract amount wanted from whats in stock
-                price *= amountWanted; // times it by the price
-                // console.log(item, price);
-                console.log(chalk`{bold.green \r\nYou just bought: ${amountWanted} ${item}'s for the price of ${price}}`); // log what they bought and how much
-                updateStore(newTotal, id); // send the new stock number and the id of the product to update store func
-            } else {
-                console.log(chalk`{red Insufficient funds!}`); // if not enough are in stock 
-            };
+                if (amountWanted <= inStock) { // if the amount wanted is less than or equal to whats in stock
+                    let newTotal = inStock - amountWanted; // subtract amount wanted from whats in stock
+                    price *= amountWanted; // times it by the price
+                    // console.log(item, price);
+                    console.log(chalk`{bold.green \r\nYou just bought: ${amountWanted} ${item}'s for the price of ${price}}`); // log what they bought and how much
+                    updateStore(newTotal, id); // send the new stock number and the id of the product to update store func
+                } else {
+                    console.log(chalk`{red We don't have that many!}`); // if not enough are in stock 
+                };
+            });
         });
-    });
+    })
 };
 
 const updateStore = (newTotal, id) => { // update the store with the new stock quantity 
